3931e05dc66a1a4ce581e3a7664f61db6eb73d11
EkzekutorGraz <60058441+EkzekutorGraz@users.noreply.github.com>
2020-07-19 02:13:46 +0300

    генерация карты

--- MapManager.cs
+++ MapManager.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System; 
 
 public class MapManager
 {
    public static Tile[,] map; //2х мерная карта с информацией о тайлах
 }
 
+[Serializable] //
 public class Tile { //содержит информацию каждого отдельного тайла
     public int xPosition; //позиция по оХ
     public int yPosition; //позиция по оУ
+    [NonSerialized]
     public GameObject baseObject; //содержит информацию о объекте и его позиции
 }
